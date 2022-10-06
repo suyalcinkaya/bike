abfa5f42640a239f4acb516917dc896a9e91d4fd
suyalcinkaya <onur.suyalcinkaya@gmail.com>
2022-10-06 14:31:56 +0200

    chore: reset safari seaerch input styling

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -3,7 +3,7 @@ import { IPageLayoutProps } from 'layouts/PageLayout.types'
 
 const PageLayout = (props: IPageLayoutProps) => {
   return (
-    <main className="flex min-h-screen pb-16 pt-16 overflow-hidden px-safe">
+    <main className="flex min-h-screen py-8 md:py-16 overflow-hidden px-safe">
       <div className="px-6 md:px-16 mx-auto w-full max-w-screen-lg">
         <div className="h-full" {...props} />
       </div>
